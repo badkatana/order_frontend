65e4c438ff6df28d7b4452714d272100b8667c75
badkatana <badgoodkatana@gmail.com>
2025-01-12 02:36:27 +0500

    16

--- src/widgets/project/ProjectItem/ProjectItem.tsx
+++ src/widgets/project/ProjectItem/ProjectItem.tsx
@@ -4,29 +4,55 @@ import { EventForm, TaskForm } from '@/shared/formConfigs'
 import { ContainerPlaceholder } from '@/shared/ui'
 import { ListItemEvent } from '@/shared/ui/listItems/ListItemEvent'
 import { ListItemTask } from '@/shared/ui/listItems/ListItemTask'
-import { submitTask } from '@/widgets/lib/submitForm'
+import { submitEvent, submitTask } from '@/widgets/lib/submitForm'
 import { Box, styled, Typography } from '@mui/material'
 import LinearProgress from '@mui/material/LinearProgress'
 import { useQueryClient } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import { EntityArea } from './EntityArea'
 
 export const ProjectItem = ({ project }: { project?: Project; tasks?: Task[] }) => {
-	const { description, hardDeadline, priority, taskIds, id, tasks, events } = project || {}
+	const { description, hardDeadline, priority, taskIds, id, tasks, events, tasksIds } = project || {}
+	const queryClient = useQueryClient()
 
 	if (project === null) return <ContainerPlaceholder placeholder={'Select or create a project'} />
-	const queryClient = useQueryClient()
 
 	const submitProjectTask = async task => {
 		submitTask({ ...task, projectId: id })
 		queryClient.invalidateQueries(['projects'])
 	}
 
+	const submitProjectEvent = async event => {
+		submitEvent({ ...event, projectId: id })
+		queryClient.invalidateQueries(['projects'])
+	}
+
+	const [progress, setProgress] = useState(0)
+
+	useEffect(() => {
+		if (tasks && tasks.$values.length > 0) {
+			const completedTasks = tasks.$values.filter(task => task.status).length
+			const totalTasks = tasks.$values.length
+			setProgress((completedTasks / totalTasks) * 100)
+		} else {
+			setProgress(0)
+		}
+	}, [tasks])
+
 	return (
 		<ProjectCard>
 			<Box>
 				<Typography variant='h4'>{description}</Typography>
-				<Box>Tasks: {taskIds?.length}</Box>
-				<LinearProgress value={23} variant='determinate' />
+				<Box sx={{ paddingTop: '1em' }}>
+					Tasks: {taskIds?.length}
+					<LinearProgress
+						value={progress}
+						variant='determinate'
+						color='inherit'
+						sx={{ marginTop: '0.5em' }}
+					/>
+				</Box>
+
 				<Box
 					sx={{
 						display: 'flex',
@@ -46,7 +72,9 @@ export const ProjectItem = ({ project }: { project?: Project; tasks?: Task[] }) 
 						}}
 					>
 						{tasks?.$values?.length > 0 ? (
-							tasks.$values.map(task => <ListItemTask task={task} />)
+							tasks.$values.map((task, index) => (
+								<ListItemTask key={`task_project_${index}`} task={task} />
+							))
 						) : (
 							<ContainerPlaceholder placeholder={'No tasks'} />
 						)}
@@ -56,11 +84,13 @@ export const ProjectItem = ({ project }: { project?: Project; tasks?: Task[] }) 
 						title={'Events'}
 						modalConfig={{
 							config: EventForm,
-							submitFunction: submitTask,
+							submitFunction: submitProjectEvent,
 						}}
 					>
 						{events?.$values?.length > 0 ? (
-							events.$values.map(event => <ListItemEvent event={event} />)
+							events.$values.map((event, index) => (
+								<ListItemEvent key={`event_project_${index}`} event={event} />
+							))
 						) : (
 							<ContainerPlaceholder placeholder={'No events'} />
 						)}
